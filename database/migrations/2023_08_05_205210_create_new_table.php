33fe23973c2b25996c9a1c48ac6ce626be13cca1
Eng.Mohamed Maher <75702799+MOMaher891@users.noreply.github.com>
2023-08-06 00:39:15 +0300

    Migration for time&alter in center table

--- database/migrations/2023_08_05_205210_create_new_table.php
+++ database/migrations/2023_08_05_205210_create_new_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateQuizzesTable extends Migration
+class CreateNewTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,10 @@ class CreateQuizzesTable extends Migration
      */
     public function up()
     {
-        Schema::create('quizzes', function (Blueprint $table) {
+        Schema::create('times', function (Blueprint $table) {
             $table->id();
+            $table->time('from');
+            $table->time('to');
             $table->timestamps();
         });
     }
@@ -26,6 +28,6 @@ class CreateQuizzesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('quizzes');
+        Schema::dropIfExists('times');
     }
 }
