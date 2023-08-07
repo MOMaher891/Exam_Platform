202b5debfdd2b40e29aa7146bceb444781b0c8ac
Eng.Mohamed Maher <75702799+MOMaher891@users.noreply.github.com>
2023-08-07 15:23:35 +0300

    Create Staff View

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -9,12 +9,26 @@ class HomeController extends Controller
 {
     /**
      * View Functions
+     * 1) Staff View
      */
     public function index(){
         return view('dashboard.index');
     }
-    public function get_login_form(){
-        return view('dashboard.login');
-    }
 
+    /**
+     *Start Staff View
+    */
+    public function createStaff(){
+        return view('dashboard.staff.create');
+    }
+    public function editStaff(){
+        return view('dashboard.staff.edit');
+    }
+    /**
+     *End Staff View
+    */
+    /**
+     * Functional Functions
+     */
+    // Staff CRUD functions here...
 }
